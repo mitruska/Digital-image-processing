ca7eb24e99f7e86a514d10e1695094a73ce078ff
Unknown <kasiuniaczek11@interia.pl>
2018-06-06 02:07:13 +0200

    Next geo

--- Kasia/src/geometric.py
+++ Kasia/src/geometric.py
@@ -26,7 +26,7 @@ class Geometric:
 
 
         #PRZECUNIECIE Z UCIECIEM
-        result_matrix = np.empty((height, width, 3), dtype=np.uint8)
+        result_matrix = np.zeros((height, width, 3), dtype=np.uint8)
 
         for y in range(height):
             for x in range(width):  
@@ -47,7 +47,7 @@ class Geometric:
         width = image_matrix.shape[1]    # szereoksc
 
         # result_matrix = np.empty((math.ceil(height/scale_y), math.ceil(width/scale_x), 3), dtype=np.uint8)
-        result_matrix = np.empty((height, width, 3), dtype=np.uint8)
+        result_matrix = np.zeros((height, width, 3), dtype=np.uint8)
 
         # scale_height = 0
         # scale_width = 0
@@ -73,7 +73,7 @@ class Geometric:
         width = image_matrix.shape[1]    # szereoksc
 
         # result_matrix = np.empty((math.ceil(height/scale_y), math.ceil(width/scale_x), 3), dtype=np.uint8)
-        result_matrix = np.empty((height, width, 3), dtype=np.uint8)
+        result_matrix = np.zeros((height, width, 3), dtype=np.uint8)
 
         # scale_height = 0
         # scale_width = 0
@@ -157,7 +157,7 @@ class Geometric:
         height = image_matrix.shape[0]   # wysokosc
         width = image_matrix.shape[1]    # szereoksc
 
-        result_matrix = np.empty((height, width, 3), dtype=np.uint8)
+        result_matrix = np.zeros((height, width, 3), dtype=np.uint8)
         _height = height-1 #array height last index
 
         for y in range(height):
@@ -177,7 +177,7 @@ class Geometric:
         height = image_matrix.shape[0]   # wysokosc
         width = image_matrix.shape[1]    # szereoksc
 
-        result_matrix = np.empty((height, width, 3), dtype=np.uint8)
+        result_matrix = np.zeros((height, width, 3), dtype=np.uint8)
         _width = width - 1 #array width last index
 
         for y in range(height):
@@ -192,6 +192,118 @@ class Geometric:
         if save == True:
             Image.fromarray(result_matrix, "RGB").save("../../Resources/Color/Color_Scale_Result.tiff", "TIFF")  
 
+    def sym_paramy(self, param_y = 0, show = False, save = False):
+        image_matrix = self.im1
+        height = image_matrix.shape[0]   # wysokosc
+        width = image_matrix.shape[1]    # szereoksc
+
+        result_matrix = np.zeros((height, width, 3), dtype=np.uint8)
+        _height = height - 1 #array height last index
+ 
+        param_y = height/2
+
+        for y in range(height):
+            for x in range(width): 
+                if y < param_y:
+                    result_matrix[y][x] = image_matrix[y][x]
+                else:
+                    result_matrix[y][x] = image_matrix[_height - y][x]
+
+        if show == True:
+            #przed 
+            Image.fromarray(image_matrix).show()  
+            #po    
+            Image.fromarray(result_matrix).show() 
+        if save == True:
+            Image.fromarray(result_matrix, "RGB").save("../../Resources/Color/Color_Scale_Result.tiff", "TIFF")  
+
+
+
+
+    def sym_paramx(self, param_x = 0, show = False, save = False):
+        image_matrix = self.im1
+        height = image_matrix.shape[0]   # wysokosc
+        width = image_matrix.shape[1]    # szereoksc
+
+        result_matrix = np.zeros((height, width, 3), dtype=np.uint8)
+        _width = width - 1 #array width last index
+ 
+        param_x = width/2
+
+        for y in range(height):
+            for x in range(width): 
+                if x < param_x:
+                    result_matrix[y][x] = image_matrix[y][x]
+                else:
+                    result_matrix[y][x] = image_matrix[y][_width - x]
+        
+        if show == True:
+            #przed 
+            Image.fromarray(image_matrix).show()  
+            #po    
+            Image.fromarray(result_matrix).show() 
+        if save == True:
+            Image.fromarray(result_matrix, "RGB").save("../../Resources/Color/Color_Scale_Result.tiff", "TIFF")  
+
+    def cut_frg(self, x_min, x_max, y_min, y_max, show = False, save = False):
+        
+        image_matrix = self.im1
+        height = image_matrix.shape[0]   # wysokosc
+        width = image_matrix.shape[1]    # szereoksc
+
+        result_matrix = np.zeros((height, width, 3), dtype=np.uint8)
+
+        for y in range(height):
+            for x in range(width): 
+                #poruszamy się w pierwszej ćwiartce osi układu współrzędnych
+                if x > x_min and x < x_max and y < height-y_min and y > height-y_max:
+                    result_matrix[y][x] = 0
+                else:
+                    result_matrix[y][x] = image_matrix[y][x]
+        
+        if show == True:
+            #przed 
+            Image.fromarray(image_matrix).show()  
+            #po    
+            Image.fromarray(result_matrix).show() 
+        if save == True:
+            Image.fromarray(result_matrix, "RGB").save("../../Resources/Color/Color_Scale_Result.tiff", "TIFF")  
+
+    def copy_frg(self, x_min, x_max, y_min, y_max, show = False, save = False):
+        
+        image_matrix = self.im1
+        height = image_matrix.shape[0]   # wysokosc
+        width = image_matrix.shape[1]    # szereoksc
+
+        result_matrix = np.zeros((height, width, 3), dtype=np.uint8)
+        
+        #macierz o wymiarach wycinanego fragmentu
+        cut_matrix = np.zeros((y_max-y_min + 1, x_max-x_min + 1, 3), dtype=np.uint8)
+
+        cut_y = 0
+        for y in range(height):
+            cut_x = 0
+            for x in range(width): 
+                #poruszamy się w pierwszej ćwiartce osi układu współrzędnych
+                if x >= x_min and x <= x_max and y <= height-y_min and y >= height-y_max:    
+                    result_matrix[y][x] = image_matrix[y][x]
+                    cut_matrix[cut_y][cut_x] = image_matrix[y][x]
+                    cut_x+=1
+            if cut_x > 0:    
+                cut_y+=1
+
+        
+        
+        if show == True:
+            #przed 
+            Image.fromarray(image_matrix).show()  
+            #po    
+            Image.fromarray(result_matrix).show() 
+            Image.fromarray(cut_matrix).show()  
+
+        if save == True:
+            Image.fromarray(result_matrix, "RGB").save("../../Resources/Color/Color_Scale_Result.tiff", "TIFF")  
+
 
 
 geo = Geometric(image1Path = "../../Resources/Color/Warzywa.tiff", image2Path = "../../Resources/Color/Szympans.tiff" )
@@ -200,4 +312,6 @@ geo = Geometric(image1Path = "../../Resources/Color/Warzywa.tiff", image2Path = 
 # geo.scale_j(5, True, True)
 # geo.turn(20, True, True)
 # geo.sym_x(True, True)
-geo.sym_y(True, True)
+# geo.sym_paramx(True, True)
+# geo.cut_frg(5,20,40,80, True, True)
+geo.copy_frg(5,20,40,80, True, True)
